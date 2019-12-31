0bcf232227a2f59ef995cec14346904d59c2286c
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2019-12-31 18:23:44 +0100

    update postFiles with name obfuscation and output file
    Former-commit-id: 5e2650002d329d6869392114991077c7a4c1c507
    Former-commit-id: 55b8894e2559653f5e49feffaca5c6bf486a3aa2

--- postFiles.sh
+++ postFiles.sh
@@ -3,6 +3,9 @@
 # Script to upload a file on Usenet using ngPost (https://github.com/mbruel/ngPost)
 # it's goal it's to do the split and the par2 generation in a temp folder prior to upload it
 #
+UUID_NAME=1
+GEN_UUID=dbus-uuidgen
+
 QT_VERSION=5.12.6
 SCRIPT=`readlink -f "$0"`
 SCRIPT_PATH=`dirname "$SCRIPT"`
@@ -10,27 +13,41 @@ LIBS="$SCRIPT_PATH/libs"
 
 export QT_PLUGIN_PATH=$LIBS/plugins/
 
+NG_POST="$SCRIPT_PATH/src/ngPost"
+TMP_FOLDER=/tmp
+
 # Uncomment the line below in case there is an issue loading the plugins
 #export QT_DEBUG_PLUGINS=1
 
 export LD_LIBRARY_PATH=$LIBS:$LD_LIBRARY_PATH
 
-NG_POST="$SCRIPT_PATH/bin/ngPost"
-TMP_FOLDER=/tmp
-
+# 0.: if using RAR obfuscation, generate RAR_NAME
+if [ "$UUID_NAME" -eq 1 ]; then
+	RAR_NAME=`$GEN_UUID`
+	if [ "$?" -ne 0 ]; then
+		echo "can't find the '$GEN_UUID' command to generate the RAR name..."
+		exit -1
+		# could try using pwgen instead ;)
+	else
+		echo "using RAR obfuscation: $RAR_NAME.rar"
+	fi
+fi
 
 syntax()
 {
-	echo "syntax: $0 (-c <ngPost config file>)? (-i <input file>)+"
+	echo "syntax: $0 (-c <ngPost config file>)? (-i <input file>)+ (-o <nzb file>)?"
 }
 
 #1.: parse the options
-while getopts "hc:i:" opt
+while getopts "hc:i:o:" opt
 do
 	case $opt in
 	"i")
 #		FILE="$OPTARG"
 		FILES+=("$OPTARG")
+	;;
+	"o")
+		NZBFILE="$NZB_PATH/$OPTARG"
 	;;
 	"c")
 		CONFIG="$OPTARG"
@@ -49,7 +66,6 @@ done
 
 MAIN_FILE=$FILES
 echo "Nb files: ${#FILES[@]}, main one: $MAIN_FILE, all files: ${FILES[@]}"
-
 
 #2.: validate the options
 #if [ -z $CONFIG ] || [ ! -f $CONFIG ] ; then
@@ -73,25 +89,42 @@ fi
 mkdir $TMP_FOLDER
 
 
+#4.: if no RAR_NAME (no obfuscation) use real FILENAME
+if [ -z "$RAR_NAME" ]; then
+	RAR_NAME=$FILENAME
+fi
+
+
 #4.: create the archives and par2
 PASS=$(pwgen -cBsn 13 1)
 echo "RAR pass: $PASS"
-rar a -v50m -ed -ep1 -m0 -hp"$PASS" "$TMP_FOLDER/$FILENAME.rar" "${FILES[@]}"
+rar a -v50m -ed -ep1 -m0 -hp"$PASS" "$TMP_FOLDER/$RAR_NAME.rar" "${FILES[@]}"
 cd $TMP_FOLDER
 par2 c -s768000 -r8 *.rar
 
 
 
-#5.: post TMP_FOLDER
-#echo "$NG_POST -m "password=$PASS" -i $TMP_FOLDER"
-if [ -z $CONFIG ] ; then
-	$NG_POST -m "password=$PASS" -i $TMP_FOLDER
-else
-	$NG_POST -m "password=$PASS" -c "$CONFIG" -i $TMP_FOLDER
+#5.: create the NZBFILE if not provided (using main FILENAME)
+if [ -z "$NZBFILE" ]; then
+	NZBFILE="$NZB_PATH/$FILENAME"
 fi
 
 
-#6.: clean TMP_FOLDER
+#6.: generate random from
+FROM="$(pwgen -cBsn 11 1)@ngPost.com"
+echo "RAR_NAME: $RAR_NAME, PASS: $PASS, NZBFILE: $NZBFILE, FROM: $FROM"
+
+
+#7.: post TMP_FOLDER
+#echo "$NG_POST -m "password=$PASS" -i $TMP_FOLDER"
+if [ -z $CONFIG ] ; then
+	$NG_POST -m "password=$PASS" -i $TMP_FOLDER -o $NZBFILE -f $FROM
+else
+	$NG_POST -m "password=$PASS" -c "$CONFIG" -i $TMP_FOLDER -o $NZBFILE -f $FROM
+fi
+
+
+#8.: clean TMP_FOLDER
 rm -rf $TMP_FOLDER
 
 exit 0
