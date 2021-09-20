add15ac7c6090b11f885a4c31b36eaa41fbda3e7
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2021-09-21 01:53:33 +0200

    fix makeAppImage.sh

--- src/makeAppImage.sh
+++ src/makeAppImage.sh
@@ -4,7 +4,9 @@ make clean
 $MB_QMAKE -o Makefile ngPost.pro CONFIG+=release
 make -j 2
 cp ngPost ../release/ngPost/
-cd ../..
+cd ..
+cp  ngPost.conf release_notes.txt README* release/ngPost/
+cd ..
 /home/mb/apps/bin/linuxdeployqt-6-x86_64.AppImage ngPost/ngPost.desktop -appimage -qmake=$MB_QMAKE
 ls -alrt
 
