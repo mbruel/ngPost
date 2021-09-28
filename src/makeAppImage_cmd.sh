d67bc1a10b4d57fb89ddd7fe98be540e6451434a
mb <mb@tieum.ddns.net>
2021-09-28 16:02:36 +0200

    Issue #120: building cmd only Linux version

---
+++ src/makeAppImage_cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git pull
+make clean
+$MB_QMAKE -o Makefile ngPost_cmd.pro CONFIG+=release
+make -j 2
+cp ngPost ../release/ngPost/
+cd ..
+rm -rf release/ngPost/lib release/ngPost/plugins
+cp  ngPost.conf release_notes.txt README* release/ngPost/
+cd ..
+/home/mb/apps/bin/linuxdeployqt-6-x86_64.AppImage ngPost/ngPost.desktop -appimage -qmake=$MB_QMAKE
+ls -alrt
+
